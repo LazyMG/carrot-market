d9c25e84d19c57634340f241b1c03ef817a3e40c
LazyMG <cbfmark@gmail.com>
2023-11-22 00:03:20 +0900

    #14.4

--- pages/streams/[id].tsx
+++ pages/streams/[id].tsx
@@ -22,9 +22,10 @@ interface StreamWithMessages extends Stream {
   messages: StreamMessage[];
 }
 
+//에러 처리 필요
 interface StreamResponse {
-  ok: boolean;
-  stream?: StreamWithMessages;
+  ok: true; //ok:boolean
+  stream: StreamWithMessages; //stream?: StreamWithMessages;
 }
 
 interface MessageForm {
@@ -36,7 +37,8 @@ const StreamDetail: NextPage = () => {
   const router = useRouter();
   const { register, handleSubmit, reset } = useForm<MessageForm>();
   const { data, mutate } = useSWR<StreamResponse>(
-    router.query.id ? `/api/streams/${router.query.id}` : null
+    router.query.id ? `/api/streams/${router.query.id}` : null,
+    { refreshInterval: 1000 }
   );
   const [sendMessage, { loading, data: sendMessageData }] = useMutation(
     `/api/streams/${router.query.id}/messages`
@@ -45,14 +47,23 @@ const StreamDetail: NextPage = () => {
   const onValid = (form: MessageForm) => {
     if (loading) return;
     reset();
+    mutate(
+      (prev) =>
+        prev &&
+        ({
+          ...prev,
+          stream: {
+            ...prev.stream,
+            messages: [
+              ...prev.stream?.messages,
+              { id: Date.now(), message: form.message, user: { ...user } },
+            ],
+          },
+        } as any),
+      false
+    );
     sendMessage(form);
   };
-
-  useEffect(() => {
-    if (sendMessageData && sendMessageData.ok) {
-      mutate();
-    }
-  }, [sendMessageData, mutate]);
 
   return (
     <Layout canGoBack>
