5b2ec580df112128f886bd9f56b91d2ec07e294c
LazyMG <cbfmark@gmail.com>
2023-11-23 23:57:55 +0900

    #15

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout";
 import Input from "../../components/input";
 import Button from "../../components/button";
@@ -13,6 +13,7 @@ interface EditProfileForm {
   phone?: string;
   name?: string;
   formErrors?: string;
+  avatar?: FileList;
 }
 
 interface EditProfileResponse {
@@ -29,30 +30,54 @@ const EditProfile: NextPage = () => {
     setError,
     formState: { errors },
     clearErrors,
+    watch,
   } = useForm<EditProfileForm>();
   const router = useRouter();
+  const [avatarPreview, setAvatarPreview] = useState("");
 
   useEffect(() => {
     if (user?.email) setValue("email", user.email);
     if (user?.phone) setValue("phone", user.phone);
     if (user?.name) setValue("name", user.name);
+    if (user?.avatar)
+      setAvatarPreview(
+        `https://imagedelivery.net/gZGVczSRdOSDlrmytjMYIA/${user?.avatar}/avatar`
+      );
   }, [user, setValue]);
 
   const [editProfile, { data, loading }] = useMutation<EditProfileResponse>(
     `/api/users/me`
   );
-  const onValid = ({ email, phone, name }: EditProfileForm) => {
+  const onValid = async ({ email, phone, name, avatar }: EditProfileForm) => {
     if (loading) return;
     if (email === "" && phone === "" && name == "") {
       return setError("formErrors", {
         message: "Email Or Phone number are required. You need to choose one.",
       });
     }
-    editProfile({
-      email,
-      phone,
-      name,
-    });
+    if (avatar && avatar.length > 0 && user) {
+      const { id, uploadURL } = await (await fetch(`/api/files`)).json();
+      const form = new FormData();
+      form.append("file", avatar[0], user?.id.toString());
+      const request = await (
+        await fetch(uploadURL, {
+          method: "POST",
+          body: form,
+        })
+      ).json();
+      editProfile({
+        email,
+        phone,
+        name,
+        avatarId: id,
+      });
+    } else {
+      editProfile({
+        email,
+        phone,
+        name,
+      });
+    }
   };
   useEffect(() => {
     if (data && !data.ok) {
@@ -62,7 +87,7 @@ const EditProfile: NextPage = () => {
 
   useEffect(() => {
     if (data?.ok === true) {
-      router.push(`/users/profile`);
+      router.push(`/profile`);
     }
   }, [data, router]);
 
@@ -71,6 +96,14 @@ const EditProfile: NextPage = () => {
       clearErrors("formErrors");
     }
   };
+  const avatar = watch("avatar");
+
+  useEffect(() => {
+    if (avatar && avatar.length > 0) {
+      const file = avatar[0];
+      setAvatarPreview(URL.createObjectURL(file));
+    }
+  }, [avatar]);
 
   return (
     <Layout canGoBack title="Edit Profile">
@@ -80,13 +113,21 @@ const EditProfile: NextPage = () => {
         className="py-10 px-4 space-y-4"
       >
         <div className="flex items-center space-x-3">
-          <div className="w-14 h-14 rounded-full bg-slate-500" />
+          {avatarPreview !== "" ? (
+            <img
+              src={avatarPreview}
+              className="w-14 h-14 rounded-full bg-slate-500"
+            />
+          ) : (
+            <div className="w-14 h-14 rounded-full bg-slate-500" />
+          )}
           <label
             htmlFor="picture"
             className="cursor-pointer py-2 px-3 border hover:bg-gray-50 border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700"
           >
             Change
             <input
+              {...register("avatar")}
               id="picture"
               type="file"
               className="hidden"
